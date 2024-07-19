39ffde0d7aa5accc155676c4064c096b1296ca37
michailaja <michailaja@gmail.com>
2024-07-19 01:39:23 +0000

    reafature: change after createUser

--- lib/services/authServices.dart
+++ lib/services/authServices.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+import 'package:myapp/auth/login.dart';
 import 'package:myapp/pages/home.dart';
 
 class Authservices {
@@ -14,7 +15,7 @@ class Authservices {
 
       await Future.delayed(const Duration(seconds: 1));
       Navigator.pushReplacement(context,
-          MaterialPageRoute(builder: (BuildContext context) => const Home()));
+          MaterialPageRoute(builder: (BuildContext context) => const Login()));
     } on FirebaseAuthException catch (e) {
       String message = '';
       if (e.code == 'weak-password') {
